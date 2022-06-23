306dde9444e7303c45f3b84a035db1c4f8aac8cd
Mustafa Iram <mustafaudoy@gmail.com>
2022-06-22 18:26:16 -0700

    hello

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Welcome } from "./components/Welcome";
+import { CssBaseline } from '@mui/material';
+import makeStyles from "@mui/material";
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+            
+             <Welcome />
+
+    </div>
+  );
+}
+
+export default App;
