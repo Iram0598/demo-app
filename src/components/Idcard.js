16d4bb95d132f16f7a480e4a3a956c09ee7bc940
Mustafa Iram <mustafaudoy@gmail.com>
2022-09-18 13:04:54 +0600

    updated

--- src/components/Idcard.js
+++ src/components/Idcard.js
@@ -1,59 +1,50 @@
 import React from 'react'
+import { IdcardBack } from './IdcardBack'
 
 
 export const Idcard = () => {
   return (
+      <>
     //Main box
     <div className='border border-primary h- d-inline-block  w-75 p-3'>
         <div className='title'>
+        <div className='text-center'>   
+        <img src={`${process.env.PUBLIC_URL}/images/1567836534377.jpg`}  alt="logo" className='img-circle w-25 h-25 d-inline-block'></img>
+        </div>   
              <h1 className='text-center'><b>Arunima Group</b></h1>
-             <h3 className='text-center'>Dewan Idris Road, Zirabo, <br>
-             </br>Savar, Dhaka - 1341</h3>
+            
         </div>
         <div className='mt-5 mb-5'>
-              <h2 className='text-center'>Arunima Sportswear Ltd.</h2>
+              <h4 className='text-center'>Arunima Sportswear Ltd.</h4>
               
         </div>
+        <div className='text-center'>   
+        <img src={`${process.env.PUBLIC_URL}/images/frontal-male-passport-photo-isolated-white-background-eu-standardization-frontal-male-passport-photo-isolated-white-149548031.jpg`}  alt="logo" className='border  border-5 border-dark rounded-circle w-25'></img>
+        </div> 
          {/* Info box */}
-        <div className='d-flex'> 
-        <div className='me-5 ms-4'>
-              <h4>Name</h4>
-              <h4>Id Card No</h4>
-              <h4>Issue Date</h4>
-              <h4>Designation </h4>
-              <h4>Department</h4>
-              <h4>Nature of work</h4>
-              <h4>Ticket/Card No</h4>
-              <h4>Joining Date</h4>
-              <h4>Blood group</h4>
-
+        <div className='text-center'> 
+                  <h2 className='mt-4'><b>Bappi Datta</b></h2>
+                  <h4 className='text-primary'>CEO</h4>
+        
         </div>
-        <div className='ms-5'>
-              <h6>:Mustafa Iram Udoy</h6>
-              <h6>:1706401</h6>
-              <h6>:22/01/22</h6>
-              <h6>:Frontend Developer<br></br>
-              </h6>
-              <h6>:IT</h6>
-              <h6>:</h6>
-              <h6>:12744</h6>
-              <h4>:15 April, 2009</h4>
-              <h4>:O-</h4>
+        <div className='text-center mt-4'>
+               <h4>Expire date: 08/22</h4>
         </div>
+        <div className='mt-5'>
+               <h3 className='text-center p-3 mb-2 bg-dark text-white  '>IT Department</h3>
         </div>
-        <div className='d-flex justify-content-evenly mt-5'>
-        <div className='ms-4 me-5'>
-               <image>Image</image>
-               <h4 className=' border-top border-dark'>Signature</h4>
-        </div>
-        <div className='ms-5 ms-5'>
-        <image>Image</image>
-               <h4 className='border-top border-dark '>Signature</h4>
-        </div>
-        </div>
-
-
     </div>
+    <IdcardBack/>
+    
+    </>
+
+    
+    
+
+
+    
+
+
 
   )
 }
---
+++ src/components/IdcardBack.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const IdcardBack = () => {
+  return (
+    <div></div>
+  )
+}
